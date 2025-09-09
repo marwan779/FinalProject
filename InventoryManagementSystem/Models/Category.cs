bf35b6064b2cbb24605373e17582ce1b4706fa8d
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-09-09 14:00:47 +0300

    Add Category and Product Models

---
+++ InventoryManagementSystem/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagementSystem.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ InventoryManagementSystem/Models/Product.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryManagementSystem.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [MaxLength(50)]
+        public string SKU { get; set; }
+
+        [MaxLength(100)]
+        public string? Barcode { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal CostPrice { get; set; }
+
+        public int QuantityInStock { get; set; }
+
+        public int ReorderLevel { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //public ICollection<PurchaseOrderItem> PurchaseOrderItems { get; set; } = new List<PurchaseOrderItem>();
+        //public ICollection<SaleOrderItem> SaleOrderItems { get; set; } = new List<SaleOrderItem>();
+        //public ICollection<InventoryTransaction> InventoryTransactions { get; set; } = new List<InventoryTransaction>();
+
+    }
+}
