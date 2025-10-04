ec83f0c6c4834791185fc17849124ffdc82238c4
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-10-04 13:53:48 +0300

    Creating productVM

---
+++ InventoryManagementSystem/Models/ViewModels/Product/ProductVM.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagementSystem.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryManagementSystem.Models.ViewModels.Product
+{
+    public class ProductVM
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal CostPrice { get; set; }
+        public int QuantityInStock { get; set; }
+        public bool IsDamaged { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
