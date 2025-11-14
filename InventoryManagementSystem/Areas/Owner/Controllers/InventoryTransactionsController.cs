de7bc04f990949f01ecf2cdf80a59b423b079fb6
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-11-14 11:24:58 +0200

    Finshing transaction index view

---
+++ InventoryManagementSystem/Areas/Owner/Controllers/InventoryTransactionsController.cs
@@ -0,0 +1,48 @@
+﻿using InventoryManagementSystem.DataAccess.Repository.IRepository;
+using InventoryManagementSystem.Models.Entities;
+using InventoryManagementSystem.Models.ViewModels.InventoryTransaction;
+using InventoryManagementSystem.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementSystem.Areas.Owner.Controllers
+{
+    [Area(StaticDetails.OwnerRole)]
+    [Authorize(Roles = StaticDetails.OwnerRole)]
+    public class InventoryTransactionsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public InventoryTransactionsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<InventoryTransactionVM> transactions = new List<InventoryTransactionVM>();
+
+            List<InventoryTransaction> inventoryTransactions = _unitOfWork.InventoryTransactionRepository
+                .GetAll(IncludeProperties: "Product").ToList();
+
+            if(inventoryTransactions.Any())
+            {
+                foreach (var trans in inventoryTransactions)
+                {
+
+                    transactions.Add(new InventoryTransactionVM()
+                    {
+                        ProductId = trans.ProductId,
+                        ProductName = trans.Product.Name,
+                        QuantityChanged = trans.QuantityChanged,
+                        TransactionType = trans.TransactionType,
+                        ReferenceId = trans.ReferenceId,
+                        CreatedAt = trans.CreatedAt,
+                    });
+                }
+            }
+            
+            return View(transactions);
+        }
+    }
+}
---
+++ InventoryManagementSystem/Models/ViewModels/InventoryTransaction/InventoryTransactionVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagementSystem.Models.ViewModels.InventoryTransaction
+{
+    public class InventoryTransactionVM
+    {
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        public int QuantityChanged { get; set; }
+
+        public string TransactionType { get; set; } = string.Empty;
+
+        public int? ReferenceId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
